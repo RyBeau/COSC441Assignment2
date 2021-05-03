3df886cd94096a043980ee2d2c78579dbb85ba41
Juliet Samandari <jsa117@cs18199kq.canterbury.ac.nz>
2021-05-03 15:30:15 +1200

    Add gates to initialize

--- src/CsmaMac.cc
+++ src/CsmaMac.cc
@@ -4,12 +4,17 @@
 Define_Module(CsmaMac);
 
 void CsmaMac::initialize () {
-    ownAddress = par("ownAddress");                       // own node address
-    bufferSize = par("bufferSize");                       // max number of application messages the MAC can hold
-    maxBackoffs = par("maxBackoffs");                      // max number of backoff operations per attempt
-    maxAttempts = par("maxAttempts");                      // max number of attempts
-    macOverheadSizeData = par("macOverheadSizeData");  // Overhead for a MAC data packet
-    macOverheadSizeAck = par("macOverheadSizeAck");   // Overhead for a MAC acknowledgement
-    macAckDelay = par("macAckDelay");             // fixed waiting time for sending ACK
-    ackTimeout = par("ackTimeout");              // timeout for ACK packet
+    ownAddress = par("ownAddress");
+    bufferSize = par("bufferSize");
+    maxBackoffs = par("maxBackoffs");
+    maxAttempts = par("maxAttempts");
+    macOverheadSizeData = par("macOverheadSizeData");
+    macOverheadSizeAck = par("macOverheadSizeAck");
+    macAckDelay = par("macAckDelay");
+    ackTimeout = par("ackTimeout");
+    fromHigherId = findGate("fromHigher");
+    toHigherId = findGate("toHigher");
+    fromTransceiverId = findGate("fromTransceiver");
+    toTransceiverId = findGate("toTransceiver");
+
 }
